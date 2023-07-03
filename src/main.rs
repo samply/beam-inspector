599c8ef616f7728724aaa9c1c13d774550553654
Jan Skiba <jan.skiba@dkfz-heidelberg.de>
2023-07-03 15:44:16 +0200

    Add proper config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
+use std::{net::SocketAddr, str::FromStr};
+
+use clap::Parser;
+use hyper::Uri;
+
 mod serve;
+
+#[derive(Debug, Parser)]
+#[clap(name("🌈 Samply.Beam.Inspector"))]
+struct Config {
+
+    /// Local bind address
+    #[clap(long, env, value_parser, default_value_t = SocketAddr::from_str("0.0.0.0:8081").unwrap())]
+    pub bind_addr: SocketAddr,
+
+    /// Beam proxy base url 
+    #[clap(long, env, value_parser)]
+    pub beam_proxy_url: Uri,
+
+    /// Beam proxy monitoring api key
+    #[clap(long, env, value_parser)]
+    pub api_key: String,
+}
+
+
 
 #[tokio::main]
 async fn main() {
-    axum::Server::bind(&"127.0.0.1:1337".parse().unwrap()).serve(serve::router("http://localhost:8081", "Maintenance".into()).into_make_service()).await.unwrap();
+    let config = Config::parse();
+    axum::Server::bind(&config.bind_addr)
+        .serve(serve::router(config.beam_proxy_url, config.api_key).into_make_service())
+        .await
+        .expect("Error starting server");
 }
--- src/serve.rs
+++ src/serve.rs
@@ -1,5 +1,5 @@
 use axum::{Router, routing::get, http::{Uri, header, StatusCode}, response::{IntoResponse, Response}, body::{boxed, Full}};
-use hyper::{Client, client::HttpConnector, Request, Body};
+use hyper::{Client, client::HttpConnector, Request, Body, http::uri::{Parts, PathAndQuery}};
 use once_cell::sync::Lazy;
 use rust_embed::RustEmbed;
 
@@ -11,8 +11,12 @@ const INDEX_HTML: &str = "index.html";
 
 static CLIENT: Lazy<Client<HttpConnector>> = Lazy::new(|| Client::new());
 
-pub fn router(beam_proxy_url: &str, monitoring_secret: String) -> Router {
-    let monitoring_endpoint: Uri = format!("{beam_proxy_url}/monitor/events").parse().expect("Invalid proxy base url");
+pub fn router(beam_proxy_url: Uri, monitoring_secret: String) -> Router {
+    let monitoring_endpoint: Uri = {
+        let mut parts = beam_proxy_url.into_parts();
+        parts.path_and_query = Some(PathAndQuery::try_from("/v1/monitor/events").unwrap());
+        Uri::from_parts(parts).unwrap()
+    };
     Router::new()
         .route("/events", get(move || async move {
             CLIENT.request(Request::get(monitoring_endpoint).header(header::COOKIE, format!("{monitoring_secret}")).body(Body::empty()).expect("Building request failed")).await.expect("Failed to make request to beam proxy")
